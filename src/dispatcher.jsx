2770337813832f5812501341f8d7833320c8f8f9
Wallace Fares <wallace.fares@webedia-group.com>
2019-05-31 10:07:43 -0300

    moved typedef

--- src/dispatcher.jsx
+++ src/dispatcher.jsx
@@ -2,11 +2,11 @@ import { useContext } from 'react';
 
 import { StoreContext } from "./Store";
 
-/** @typedef { typeof StoreContext[1] } IDispatcher */
-
 export const useDispatcher = () => {
     /** @type { IDispatcher } */
     const dispatcher = useContext(StoreContext)[1]
 
     return dispatcher
 };
+
+/** @typedef { typeof StoreContext[1] } IDispatcher */
