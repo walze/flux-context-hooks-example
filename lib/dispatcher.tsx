5ea004215cd42c963b5bfb7ebd69553d477185b3
walze <wallace.fares@webedia-group.com>
2019-11-21 14:13:53 -0300

    start

--- lib/dispatcher.tsx
+++
@@ -1,4 +0,0 @@
-import { EE, IDispatch, EVENTS } from './EventEmitter'
-
-
-export const dispatch = <T, _ = {}>(action: IDispatch<T>) => { EE.emit(EVENTS.DISPATCH, action) }
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+export const App = () => {
+  console.log('app rendered')
+
+  return (
+    <div>
+      asdf
+    </div>
+  )
+}
